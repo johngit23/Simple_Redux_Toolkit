9f8e632cb27e2e05ff19b5586b5f2c3b13617de3
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-05-31 11:21:01 +0300

    Toolkit, frontend

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotal } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+
+function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
